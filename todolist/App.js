4428c663076583c54256637c8e0d11a5264dddd0
Jessica Cockerell <jlpeters211@gmail.com>
2023-11-30 21:14:42 -0600

    Outline basic structure

--- todolist/App.js
+++ todolist/App.js
@@ -1,20 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import { View, Text } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <SafeAreaView>
+        <View>
+          <Text>Header</Text>
+        </View>
+
+        <View>
+          <Text>Body</Text>
+        </View>
+
+        <View>
+          <Text>Footer</Text>
+        </View>
+        
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
